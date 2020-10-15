48625c8cbfc0cf91d741f816102de6563f3fd603
Radosław Gąsior <radogas429@student.polsl.pl>
2020-10-15 12:43:46 +0200

    power function

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -15,28 +15,46 @@ bool is_prime(int number) {
 }
 
 int read_int() {
-	int number;
+	int n;
 
-	cout << "Enter number fo testing" << endl;
-	cin >> number;
+	cout << "Enter number for testing" << endl;
+	cin >> n;
 
-	return number;
+	return n;
 }
+
+
+int pow(int base, int power) {
+	int res = 1;
+
+	for (int i = 0; i < power; i++) {
+		res *= base;
+	}
+
+	return res;
+}
+
 
 
 int main() {
 
-	//prime testing function
+	int base = read_int();
+	int power = read_int();
+	int result = pow(base, power);
 
+	cout << "result is:" << result << endl;
+
+
+	/*
 	int number = read_int();
-
+	
 	if (is_prime(number)) {
 		cout << "Number is prime" << endl;
 	}
 	else {
 		cout << "Number isnt prime" << endl;
 	}
-	
+	*/
 
 	return 0;
 }
