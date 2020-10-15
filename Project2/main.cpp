63e71eed64a731a2ef61d8935149f8b5d186be25
Radosław Gąsior <radogas429@student.polsl.pl>
2020-10-15 12:27:05 +0200

    Prime number testing - functions

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -2,24 +2,41 @@
 
 using namespace std;
 
-int main() {
 
-	//prime testing function
+bool is_prime(int number) {
+	for (int i = 2; i < number; i++) {
+		if (number % i == 0) {
+			return false;
+		}
 
+	}
+
+	return true;
+}
+
+int read_int() {
 	int number;
 
 	cout << "Enter number fo testing" << endl;
 	cin >> number;
 
-	for (int i = 2; i < number; i++) {
-		if (number % i == 0) {
-			cout << "Number is not prime" << endl;
-			return 0;
-		}
+	return number;
+}
 
+
+int main() {
+
+	//prime testing function
+
+	int number = read_int();
+
+	if (is_prime(number)) {
+		cout << "Number is prime" << endl;
 	}
-
-	cout << "Number is prime" << endl;
+	else {
+		cout << "Number isnt prime" << endl;
+	}
+	
 
 	return 0;
 }
